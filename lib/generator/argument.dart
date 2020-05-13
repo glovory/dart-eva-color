74b99d99f0500f1b523a19713653f2e0a3ceef38
haqqi <haqqi@glovory.com>
2020-05-14 02:42:16 +0700

    🏹 [ADD] argument class for easily manage it

---
+++ lib/generator/argument.dart
@@ -0,0 +1,11 @@
+class Argument {
+  final String input;
+  final String output;
+  final String className;
+
+  Argument({
+    this.input = 'custom-theme.json',
+    this.output = 'lib/eva_colors.dart',
+    this.className = 'EvaColors',
+  });
+}
