f9d9f032a8206ff524bc043bacf4c8df6c08b16a
haqqi <haqqi@glovory.com>
2020-05-14 06:44:31 +0700

    👩 [ADD] example output

---
+++ example/eva_colors.dart
@@ -0,0 +1,116 @@
+import 'dart:ui';
+
+import 'package:eva_color/eva_color.dart';
+
+// @autogenerate DO NOT EDIT!!!
+class MyColors {
+  static const EvaColor primary = EvaColor(0xFF3366FF, {
+    100: Color(0xFFD6E4FF),
+    200: Color(0xFFADC8FF),
+    300: Color(0xFF84A9FF),
+    400: Color(0xFF6690FF),
+    500: Color(0xFF3366FF),
+    600: Color(0xFF254EDB),
+    700: Color(0xFF1939B7),
+    800: Color(0xFF102693),
+    900: Color(0xFF091A7A),
+  });
+
+  static const EvaTransparentColor primaryTransparent = 
+      EvaTransparentColor(0x663366FF, {
+    100: Color(0x143366FF),
+    200: Color(0x283366FF),
+    300: Color(0x3D3366FF),
+    400: Color(0x513366FF),
+    500: Color(0x663366FF),
+    600: Color(0x7A3366FF),
+  });
+
+  static const EvaColor success = EvaColor(0xFF3EC43E, {
+    100: Color(0xFFE5FCD9),
+    200: Color(0xFFC6F9B4),
+    300: Color(0xFF9CED8B),
+    400: Color(0xFF74DB6A),
+    500: Color(0xFF3EC43E),
+    600: Color(0xFF2DA837),
+    700: Color(0xFF1F8D32),
+    800: Color(0xFF13712C),
+    900: Color(0xFF0B5E28),
+  });
+
+  static const EvaTransparentColor successTransparent = 
+      EvaTransparentColor(0x663EC43E, {
+    100: Color(0x143EC43E),
+    200: Color(0x283EC43E),
+    300: Color(0x3D3EC43E),
+    400: Color(0x513EC43E),
+    500: Color(0x663EC43E),
+    600: Color(0x7A3EC43E),
+  });
+
+  static const EvaColor info = EvaColor(0xFF0AB1FF, {
+    100: Color(0xFFCEFCFF),
+    200: Color(0xFF9CF3FF),
+    300: Color(0xFF6CE3FF),
+    400: Color(0xFF47D0FF),
+    500: Color(0xFF0AB1FF),
+    600: Color(0xFF0789DB),
+    700: Color(0xFF0567B7),
+    800: Color(0xFF034993),
+    900: Color(0xFF01347A),
+  });
+
+  static const EvaTransparentColor infoTransparent = 
+      EvaTransparentColor(0x660AB1FF, {
+    100: Color(0x140AB1FF),
+    200: Color(0x280AB1FF),
+    300: Color(0x3D0AB1FF),
+    400: Color(0x510AB1FF),
+    500: Color(0x660AB1FF),
+    600: Color(0x7A0AB1FF),
+  });
+
+  static const EvaColor warning = EvaColor(0xFFFFE60A, {
+    100: Color(0xFFFFFCCE),
+    200: Color(0xFFFFF89C),
+    300: Color(0xFFFFF36C),
+    400: Color(0xFFFFEE47),
+    500: Color(0xFFFFE60A),
+    600: Color(0xFFDBC307),
+    700: Color(0xFFB7A105),
+    800: Color(0xFF938003),
+    900: Color(0xFF7A6801),
+  });
+
+  static const EvaTransparentColor warningTransparent = 
+      EvaTransparentColor(0x66FFE60A, {
+    100: Color(0x14FFE60A),
+    200: Color(0x28FFE60A),
+    300: Color(0x3DFFE60A),
+    400: Color(0x51FFE60A),
+    500: Color(0x66FFE60A),
+    600: Color(0x7AFFE60A),
+  });
+
+  static const EvaColor danger = EvaColor(0xFFFF3D3A, {
+    100: Color(0xFFFFE5D7),
+    200: Color(0xFFFFC5B0),
+    300: Color(0xFFFF9E88),
+    400: Color(0xFFFF7A6B),
+    500: Color(0xFFFF3D3A),
+    600: Color(0xFFDB2A36),
+    700: Color(0xFFB71D35),
+    800: Color(0xFF931231),
+    900: Color(0xFF7A0B2F),
+  });
+
+  static const EvaTransparentColor dangerTransparent = 
+      EvaTransparentColor(0x66FF3D3A, {
+    100: Color(0x14FF3D3A),
+    200: Color(0x28FF3D3A),
+    300: Color(0x3DFF3D3A),
+    400: Color(0x51FF3D3A),
+    500: Color(0x66FF3D3A),
+    600: Color(0x7AFF3D3A),
+  });
+}
