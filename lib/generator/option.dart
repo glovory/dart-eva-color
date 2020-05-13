c55a6f124fb2bc183e0e2571877ffa20a39bed90
haqqi <haqqi@glovory.com>
2020-05-14 03:09:28 +0700

    😠 [ADD] move parser as factory from options

--- lib/generator/option.dart
+++ lib/generator/option.dart
@@ -1,3 +1,4 @@
+import 'package:args/args.dart';
 import 'package:meta/meta.dart';
 
 class GeneratorOption {
@@ -16,4 +17,54 @@ class GeneratorOption {
   })  : assert(input != null),
         assert(output != null),
         assert(className != null);
+
+  factory GeneratorOption.parseArgs(List<String> args) {
+    ArgParser parser = ArgParser();
+
+    parser.addOption(
+      'input',
+      abbr: 'i',
+      defaultsTo: GeneratorOption.defaultInput,
+      callback: (value) {
+        print('Input file is ' + value);
+      },
+      help: "Specify the json input file path.",
+    );
+
+    parser.addOption(
+      'output',
+      abbr: 'o',
+      defaultsTo: GeneratorOption.defaultOutput,
+      callback: (value) {
+        print('Output file is ' + value);
+      },
+      help: "Specify the dart output file path.",
+    );
+
+    parser.addOption(
+      'class',
+      abbr: 'c',
+      defaultsTo: GeneratorOption.defaultClassName,
+      callback: (value) {
+        print('Class name is ' + value);
+      },
+      help: "Specify the dart generated class name.",
+    );
+
+//  parser.addFlag('help', abbr: 'h', help: 'Usage help', negatable: false);
+
+    ArgResults parsedArgs = parser.parse(args);
+
+//  if (parsedArgs['help']) {
+//    stdout.writeln('Eva Design Color Converter');
+//    stdout.writeln(parser.usage);
+//    exit(0);
+//  }
+
+    return GeneratorOption(
+      input: parsedArgs['input'],
+      output: parsedArgs['output'],
+      className: parsedArgs['class'],
+    );
+  }
 }
