b1c067b52172fbebb1fd2feb9eb22065c160a9b8
haqqi <haqqi@glovory.com>
2020-05-14 02:55:46 +0700

    🧾 [ADD] static const for default

--- lib/generator/option.dart
+++ lib/generator/option.dart
@@ -1,11 +1,19 @@
+import 'package:meta/meta.dart';
+
 class Option {
+  static const String defaultInput = 'custom-theme.json';
+  static const String defaultOutput = 'lib/eva_colors.dart';
+  static const String defaultClassName = 'EvaColors';
+
   final String input;
   final String output;
   final String className;
 
   Option({
-    this.input = 'custom-theme.json',
-    this.output = 'lib/eva_colors.dart',
-    this.className = 'EvaColors',
-  });
+    @required this.input,
+    @required this.output,
+    @required this.className,
+  })  : assert(input != null),
+        assert(output != null),
+        assert(className != null);
 }
