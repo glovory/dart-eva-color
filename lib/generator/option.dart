bef2084fc9f42ec8e5e28e8f9d5c96338ed4bf97
haqqi <haqqi@glovory.com>
2020-05-14 02:45:13 +0700

    📨 [FIX] rename argument to option

--- lib/generator/option.dart
+++ lib/generator/option.dart
@@ -1,9 +1,9 @@
-class Argument {
+class Option {
   final String input;
   final String output;
   final String className;
 
-  Argument({
+  Option({
     this.input = 'custom-theme.json',
     this.output = 'lib/eva_colors.dart',
     this.className = 'EvaColors',
