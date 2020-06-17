0ec6cce7e9ed5a74069f64dda40ebb404f9c4716
Nashir <nashiruddin.abubakar@gmail.com>
2020-06-17 11:14:30 +0700

    added EvaBasicColor class

--- lib/src/swatch.dart
+++ lib/src/swatch.dart
@@ -44,3 +44,32 @@ class EvaTransparentColor extends ColorSwatch<int> {
 
   Color get shade600 => this[600];
 }
+
+class EvaBasicColor extends ColorSwatch<int>{
+  const EvaBasicColor(int primary, Map<int, Color> swatch)
+      : super(primary, swatch);
+
+
+  Color get shade100 => this[100];
+
+  Color get shade200 => this[200];
+
+  Color get shade300 => this[300];
+
+  Color get shade400 => this[400];
+
+  Color get shade500 => this[500];
+
+  Color get shade600 => this[600];
+
+  Color get shade700 => this[700];
+
+  Color get shade800 => this[800];
+
+  Color get shade900 => this[900];
+
+  Color get shade1000 => this[1000];
+
+  Color get shade1100 => this[1100];
+}
+
